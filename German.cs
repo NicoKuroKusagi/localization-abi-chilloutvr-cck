a3139d0d61f3955227aa98842f0de50409c80955
RealBlueVR <70779166+RealBlueVR@users.noreply.github.com>
2021-06-04 22:01:53 +0200

    Fixed Spelling,  Added Translations
    Fixed Spelling Mistake ("Abemelden" to "Abmelden"), Added additional translations in order from 'English.cs' until line 58.

--- German.cs
+++ German.cs
@@ -14,7 +14,48 @@ namespace ABI.CCK.Scripts.Translation
             {"ABI_UI_BUILDPANEL_HEADING_ACCOUNT_INFO", "Account Informationen"},
             {"ABI_UI_BUILDPANEL_LOGIN_CREDENTIALS_INCORRECT", "Die Login-Daten sind fehlerhaft"},
             {"ABI_UI_BUILDPANEL_LOGIN_BUTTON", "Anmelden"},
-            {"ABI_UI_BUILDPANEL_LOGOUT_BUTTON", "Abemlden"},
+            {"ABI_UI_BUILDPANEL_LOGOUT_BUTTON", "Abmelden"},
+            {"ABI_UI_BUILDPANEL_LOGOUT_DIALOG_TITLE", "Lokale Anmeldeinformationen für die CCK entfernen"},
+            {
+                "ABI_UI_BUILDPANEL_LOGOUT_DIALOG_BODY",
+                "Dies entfernt die lokal gespeicherten Anmeldeinformationen. Du musst dich danach erneut authentifizieren. Möchtest du fortfahren?"
+            },
+            {"ABI_UI_BUILDPANEL_LOGOUT_DIALOG_ACCEPT", "Ja!"},
+            {"ABI_UI_BUILDPANEL_LOGOUT_DIALOG_DECLINE", "Nein!"},
+            
+            {"ABI_UI_BUILDPANEL_UPLOADER_NO_AVATARS_FOUND", "Keine konfigurierten Avatare in der Scene gefunden. - Wurde CVRAvatar hinzugefügt?"},
+            {
+                "ABI_UI_BUILDPANEL_UPLOADER_NO_SPAWNABLES_FOUND",
+                "Keine konfigurierten spawnable objects in der Scene gefunden. - Wurde CVRSpawnable hinzugefügt?"
+            },
+            {
+                "ABI_UI_BUILDPANEL_WORLDS_WARNING_SPAWNPOINT",
+                "Deine Welt hat keine spawn points festgelegt. Bitte füge einen oder mehrere spawn points in der CVRWorld Komponente hinzu. Ansonsten wird der Standort des CVRWorld holder objects genutzt."
+            },
+            {
+                "ABI_UI_BUILDPANEL_WORLDS_INFO_REFERENCE_CAMERA",
+                "Du hast keine Referenz Kamera deiner Welt zugeordnet. Es werden die Standard-Einstellungen der Kamera genutzt."
+            },
+            {
+                "ABI_UI_BUILDPANEL_WORLDS_INFO_RESPAWN_HEIGHT",
+                "Die Respawn-Höhe ist unter -500. Wenn man aus der Map gefallen ist, wird es lange dauern bis man zum Respawn-Punkt wiederkehrt. Das ist warscheinlich nicht etwas was du willst."
+            },
+            {
+                "ABI_UI_BUILDPANEL_WORLDS_ERROR_MULTIPLE_CVR_WORLD",
+                "Es sind mehrere CVR World objects in der scene vorhanden. Die Welt wird somit nicht funktionieren. Bitte stell sicher, dass sich nur ein CVR World object in der scene befindet oder verwende unser CVRWorld-Prefab."
+            },
+             {
+                "ABI_UI_BUILDPANEL_WORLDS_ERROR_WORLD_CONTAINS_AVATAR",
+                "Geladene scenes sollten niemals avatar und world descriptor objects gleichzeitig besitzen. Bitte richte deine scenes entsprechend ein."
+            },
+            {
+                "ABI_UI_BUILDPANEL_WORLDS_ERROR_NO_CONTENT",
+                "Es wurde kein Inhalt in der aktuellen scene gefunden. Hast du vergessen einen descriptor component zu einem game object hinzuzufügen?"
+            },
+            {
+                "ABI_UI_BUILDPANEL_WORLDS_ERROR_UNITY_UNSUPPORTED",
+                "Du nutzt eine Unity-Version, welche nicht mehr unterstützt wird. Bitte nutze Unity 2019.3.1f1 (das Verwenden vom Unity Hub erleichtert das Versions-Management)."
+            },
             {"ABI_UI_BUILDPANEL_UPLOAD_WORLD_BUTTON", "Welt hochladen"},
             {"ABI_UI_BUILDPANEL_UPLOAD_AVATAR_BUTTON", "Avatar hochladen"},
             {"ABI_UI_BUILDPANEL_UPLOAD_PROP_BUTTON", "Prop hochladen"},
